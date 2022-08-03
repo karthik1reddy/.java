1958edb37313b36d4aa77e41085fb7dd11d11181
karthik1reddy <kmanchal@gitam.in>
2022-08-03 11:52:47 +0530

    conditions by bollen key word

--- src/main/java/com/example/dsa/conditions.java
+++ src/main/java/com/example/dsa/conditions.java
@@ -5,10 +5,16 @@ import static java.lang.System.*;
 public class conditions {
 
     public static void main(String[] args) {
-        int a = 9;
-        int c = 10;
-          boolean b = a == c;
-        System.out.println(b);
+       // int a = 9;
+        // int c = 10;
+         // boolean b = a == c;
+        int salary = 30000;
+        if (salary > 10000){
+            salary = salary + 2000;
+        } else if (salary < 10000) {
+           salary = salary - 20000;
+        }
+        System.out.println(salary);
     }
 
 }
