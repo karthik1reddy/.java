ca8f32b73d0b1c21ca6383c3d2c2efa394a367ab
karthik1reddy <kmanchal@gitam.in>
2022-08-03 10:39:43 +0530

    whileloops basic if condition used finding 1-10 numbers by using while loop

---
+++ src/main/java/com/example/dsa/Loops.java
@@ -0,0 +1,15 @@
+package com.example.dsa;
+
+public class Loops {
+    public static void main(String[] args) {
+       // int i = 10;
+       // if (i == 10 );
+       // System.out.println(i);
+
+        int count = 1;
+        while(count != 10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
