2e78a9c4f34147e88729720c72d04e302fea0df1
karthik1reddy <kmanchal@gitam.in>
2022-08-03 11:21:45 +0530

    for loops finding 1-40 numbers by using for loop

---
+++ src/main/java/com/example/dsa/ForLoop.java
@@ -0,0 +1,11 @@
+package com.example.dsa;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for( int count = 1;count != 40; count++){
+            System.out.println(count);
+
+        }
+    }
+
+}
