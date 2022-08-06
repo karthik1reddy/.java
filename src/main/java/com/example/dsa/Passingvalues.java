6255193114c17a0f860f7f592acbc59869d51a8c
karthik1reddy <kmanchal@gitam.in>
2022-08-06 08:40:51 +0530

    passing any value

---
+++ src/main/java/com/example/dsa/Passingvalues.java
@@ -0,0 +1,11 @@
+package com.example.dsa;
+
+public class Passingvalues {
+    public static void main(String[] args) {
+        String name = "karthik";
+        greeting(name);
+    }
+    static void greeting(String nam) {
+        System.out.println(nam);
+    }
+}
