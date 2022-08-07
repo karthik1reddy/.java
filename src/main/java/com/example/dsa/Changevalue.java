54684f582c4df70f4b1b66cacc45ccb5064327c3
karthik1reddy <kmanchal@gitam.in>
2022-08-07 07:38:23 +0530

    swapping

---
+++ src/main/java/com/example/dsa/Changevalue.java
@@ -0,0 +1,16 @@
+package com.example.dsa;
+
+
+import java.util.Arrays;
+
+public class Changevalue {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,45,6,7,5,4,6,78,9,87654};
+        change(arr);
+        System.out.println( Arrays.toString (arr));
+
+    }
+    static void change(int[]nums){
+        nums[11]= 99;
+    }
+}
--- src/main/java/com/example/dsa/Swap.java
+++ src/main/java/com/example/dsa/Swap.java
@@ -2,11 +2,22 @@ package com.example.dsa;
 
 public class Swap {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int temp = a;
+        String a = "karthik";
+        String b ="srisanth";
+        String temp = a;
         a = b;
         b = temp;
         System.out.println(a + "  " + b);
     }
 }
+  /*public static void main(String[] args) {
+       int a = 10;
+       int b= 20;
+       int temp = a;
+       a = b;
+       b = a;
+       b=temp;
+       System.out.println(a+"   "+b);
+   } */
+// variable is assained to value
+// value never used
